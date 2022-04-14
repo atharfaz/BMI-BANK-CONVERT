097c05735269f2df53c94179974bfad20f951fb8
atharfaz <101614789+atharfaz@users.noreply.github.com>
2022-04-14 10:07:33 +0700

    initialize

--- src/App.js
+++ src/App.js
@@ -1,87 +1,52 @@
-//for ui we will use tailwind css
-//add tailwind cdn in index.html
 
-import { useState } from "react";
 
-export default function App() {
-  const [height, setHeight] = useState("");
-  const [weight, setWeight] = useState("");
+import React, { Component } from 'react';
+import './App.css';
+import Alert from './component/Alert';
+// import element Alert yang telah dibuat  
 
-  const [bmiResult, setBmiResult] = useState(null);
+class App extends Component {
+  render() {
+    return (
+      <div className="App container col-sm-4">
+        <br />
+        <h4>Body Mass Index</h4>
+        <br /><br />
+        <b className="text-left">Berat</b>
+        <input type="text" className="form-control" value="" />
+        <br />
+        <b className="text-left">Tinggi</b>
+        <input type="text" className="form-control" value="" />
+        <br />
+        <button className="btn btn-info">Hitung</button>
 
-  const [status, setStatus] = useState("");
-
-  function calculateBMI() {
-    let bmi = Number(weight / (height / 100) ** 2).toFixed(2);
-    setBmiResult(bmi);
-
-    let bmiStatus = getStatus(bmi);
-
-    setStatus(bmiStatus);
-
-    setHeight("");
-    setWeight("");
+        <br /><br />
+        <h3>Hasil</h3>
+        <Alert type={this.state.type} header={this.state.header}>
+          {this.state.content}
+        </Alert>
+      </div>
+    );
   }
 
-  function getStatus(bmi) {
-    if (bmi < 18.5) return "Underweight";
-    else if (bmi >= 18.5 && bmi < 24.9) return "Normal";
-    else if (bmi >= 25 && bmi < 29.9) return "Overweight";
-    else return "Obese";
+  constructor() {
+    super();
+    this.state = {
+      type: "danger",
+      header: "Anda Gemuk",
+      content: ""
+    }
   }
 
-  return (
-    <div className="w-full max-w-xs m-10">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h1 className="text-center mb-4 text-xl"> BMI Calculator</h1>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            for="username"
-          >
-            Height
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="Height "
-            type="text"
-            placeholder="Height in cm"
-            value={height}
-            onChange={(e) => setHeight(e.target.value)}
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            for="password"
-          >
-            Weight
-          </label>
-          <input
-            className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="Weight"
-            type="Weight in kg"
-            placeholder="Weight in kg"
-            value={weight}
-            onChange={(e) => setWeight(e.target.value)}
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <button
-            className="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-            onClick={calculateBMI}
-          >
-            Calculate BMI
-          </button>
-        </div>
-        {bmiResult && (
-          <div className="mt-4">
-            <p>Your BMI is: {bmiResult} </p>
-            <p>You are currently: {status}</p>
-          </div>
-        )}
-      </form>
-    </div>
-  );
+  // fungsi untuk mengubah tipe  
+  changeTypeAlert = (event) => {
+    this.setState({ type: event.target.value });
+  }
+  // fungsi untuk mengubah header  
+  changeHeaderAlert = (event) => {
+    this.setState({ header: event.target.value });
+  }
+  // fungsi untuk mengubah content  
+
 }
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BMI from './BMI';
+import Bank from './Bank';
+import Harga from './harga';
+import Biner from './convert/biner';
+import Decimal from './convert/decimal';
+import Hexadecimal from './convert/HexaDecimal';
+import Octal from './convert/Octal';
 
-import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
+  <React.StrictMode>
+    <div>
+      <BMI />
+      <hr />
+      <Bank />
+      <hr />
+      <Harga />
+      <hr />
+      <Biner />
+      <hr />
+      <Decimal />
+      <hr />
+      <Hexadecimal />
+      <hr />
+      <Octal />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
